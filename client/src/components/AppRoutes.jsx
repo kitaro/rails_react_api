5cdf7cc738d2c465f5de9b356ef405b8413dafaf
kitaro <demir_engin@yahoo.com>
2023-12-04 22:22:15 +0300

    Basit jest testi eklendi

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -1,4 +1,3 @@
-import React from "react"
 import {Link, Route, Routes} from "react-router-dom";
 import PostsList from "../features/posts/PostsList";
 import PostDetails from "../features/posts/PostDetails";
