7a0bfe76a962678f050e1b92ccfbabf8403bc307
kitaro <demir_engin@yahoo.com>
2023-12-04 15:35:46 +0300

    Yeni gönderi formu uygulandı

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import {Link, Route, Routes} from "react-router-dom";
 import PostsList from "../features/posts/PostsList";
 import PostDetails from "../features/posts/PostDetails";
+import NewPostForm from "../features/posts/NewPostForm.jsx";
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<PostsList />} />
             <Route path="posts/:id" element={<PostDetails />} />
-            <Route path="/new" element={<h1>Yeni Haber Formu</h1>} />
+            <Route path="/new" element={<NewPostForm />} />
         </Routes>
     )
 }
